95899687e17ac98db2c44aee98a7d54597fcb6b8
Helen Prokopovich <helen.prokopovich@imbue.systems>
2024-09-01 17:55:31 +0300

    feat: Services and Properties page

---
+++ shapp/src/app/properties/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Properties: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Properties!
+    </main>        
+  )
+}
+
+export default Properties;
---
+++ shapp/src/app/services/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Services: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Services!
+    </main>        
+  )
+}
+
+export default Services;
