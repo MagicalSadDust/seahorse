73ff1d6f539377c1529b40acbd4e3532697ea077
Helen Prokopovich <helen.prokopovich@imbue.systems>
2024-09-01 18:28:47 +0300

    fix: header styles

--- shapp/src/app/components/Header.tsx
+++ shapp/src/app/components/Header.tsx
@@ -4,27 +4,27 @@ import Image from 'next/image';
 const Header: React.FC = () => {
   return (
     <header className="relative flex bg-[#1A1A1A] text-white py-0  px-0 flex-col items-center">
-      <div className="w-full">
-        <div className="flex justify-center items-center bg-[url('/images/abstractDesign.svg')] w-full h-16">
-          Discover Your Dream Property with Seahorse
-        </div>
-        <nav className="flex justify-center items-center h-16 w-full">
+      <div className="flex justify-center items-center bg-[url('/images/abstractDesign.svg')] bg-no-repeat bg-cover w-full h-16">
+        Discover Your Dream Property with Seahorse
+      </div>
+      <div className="w-full flex justify-center">
+        <nav className="flex relative justify-center items-center h-16 max-w-[120.00rem] w-full">
           <div className="flex text-lg font-bold">
             <Link href="/">
               <Image src="/images/mainLogo.png" className="absolute bottom-0 left-0" alt="Seahorse Logo" width={120} height={32} />
             </Link>
           </div>
           <div className="flex space-x-4">
-            <Link href="/" className="hover:underline">
+            <Link href="/">
               Home
             </Link>
-            <Link href="/properties" className="hover:underline">
+            <Link href="/properties">
               Properties
             </Link>
-            <Link href="/services" className="hover:underline">
+            <Link href="/services">
               Services
             </Link>
-            <Link href="/contact" className="hover:underline">
+            <Link href="/contact">
               Contact
             </Link>
           </div>
